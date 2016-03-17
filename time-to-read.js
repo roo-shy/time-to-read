4a05395a0a109de72a8e96159162d48495c3d977
Rooshy <ru_sha_nia@yahoo.ca>
2016-03-17 18:46:58 -0500

    Final Edits

--- time-to-read.js
+++ time-to-read.js
@@ -4,7 +4,6 @@ var bodyContent = document.querySelector("body").innerHTML.split(" ").length/230
 return bodyContent.toFixed(2);
 }
 
-
 var readTimeViewer = document.getElementsByClassName("time-to-read-view")[0];
 var node = document.createTextNode("Read time: " + timeToRead() + " minutes");
 readTimeViewer.appendChild(node);
